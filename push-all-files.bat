196516d03c6a878b172397980d94ccede4e31722
tahaayman116 <tahaayman116@gmail.com>
2025-09-09 21:44:27 +0300

    Initial commit with all project files

--- push-all-files.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "E:\elmeshnebb-"
-git add .
-git add -A
-git add --all
-git commit -m "Complete project upload - All files including Firebase optimizations"
-git push origin main
-pause
